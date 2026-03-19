509c7193d5792225e36d20fd9ee2e502a1e483bb
AlexFuzzy21 <adrians.veino.slaidins@gmail.com>
2026-03-19 17:44:17 +0200

    Initial commit

---
+++ www/app.js
@@ -0,0 +1,9 @@
+window.addEventListener("load", function() {
+  const loader = document.querySelector(".loader-wrapper");
+  if (loader) {
+    console.log("Loaded")
+    loader.style.transition = "opacity 0.5s ease";
+    loader.style.opacity = 0;
+    setTimeout(() => loader.style.display = "none", 500);
+  }
+});
